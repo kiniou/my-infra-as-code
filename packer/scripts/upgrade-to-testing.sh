33449d8f50d7b07200f283204daedec81a95a609
Kevin Roy <kiniou@gmail.com>
2020-08-17 22:48:00 +0200

    [packer] try to build sid box

--- packer/scripts/upgrade-to-testing.sh
+++ packer/scripts/upgrade-to-testing.sh
@@ -6,21 +6,7 @@ set -x
 export DEBCONF_FRONTEND=noninteractive
 
 cat > /etc/apt/sources.list <<-EOF
-deb http://deb.debian.org/debian testing main
-deb-src http://deb.debian.org/debian testing main
-
-deb http://deb.debian.org/debian unstable main
-deb-src http://deb.debian.org/debian unstable main
-EOF
-
-cat > /etc/apt/preferences <<-EOF
-Package: *
-Pin: release a=testing
-Pin-Priority: 900
-
-Package: *
-Pin: release a=unstable
-Pin-Priority: 800
+deb http://deb.debian.org/debian unstable main contrib non-free
 EOF
 
 apt-get update
